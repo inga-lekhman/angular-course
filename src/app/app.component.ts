451777577192182f465f141964ba87399e39eae3
inga-lekhman <inga.lekhman@gmail.com>
2024-06-08 20:11:06 +0300

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,20 @@ import { MyComponentComponent } from './components/my-component/my-component.com
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
-movie = {
-  backdrop_path: "../../src/assets/img/civil-war.jpeg",
-  id: 653346,
-  overview: "Several generations in the future following Caesar's reign, apes are now the dominant species and live harmoniously while humans have been reduced to living in the shadows. As a new tyrannical ape leader builds his empire, one young ape undertakes a harrowing journey that will cause him to question all that he has known about the past and to make choices that will define a future for apes and humans alike.",
-  release_date: "May 5, 2024",
-  title: "Kingdom of the Planet of the Apes",
-  rating: 8.5
-}
-handleAddFavorite(title: string){
-  console.log('handleAddFavorite', title);
-}
-handleAddWatchList(title: string){
-  console.log('handleAddWatchList', title);
-}
+
+  movie = {
+    backdrop_path: "assets/img/planet-apes.webp",
+    id: 653346,
+    overview: "Several generations in the future following Caesar's reign, apes are now the dominant species and live harmoniously while humans have been reduced to living in the shadows. As a new tyrannical ape leader builds his empire, one young ape undertakes a harrowing journey that will cause him to question all that he has known about the past and to make choices that will define a future for apes and humans alike.",
+    release_date: "May 5, 2024",
+    title: "Kingdom of the Planet of the Apes",
+    rating: 8.5
+  }
+  handleAddFavorite(title: string) {
+    console.log('handleAddFavorite', title);
+  }
+  handleAddWatchList(title: string) {
+    console.log('handleAddWatchList', title);
+  }
 
 }
