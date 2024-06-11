565ae0829c7a0674668b65876db0d11824eaec00
inga-lekhman <inga.lekhman@gmail.com>
2024-06-11 21:24:55 +0300

    pipes and directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyComponentComponent } from './components/my-component/my-component.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MyComponentComponent, RouterOutlet],
+  imports: [MyComponentComponent, RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
-  movie = {
-    backdrop_path: "assets/img/planet-apes.webp",
-    id: 653346,
-    overview: "Several generations in the future following Caesar's reign, apes are now the dominant species and live harmoniously while humans have been reduced to living in the shadows. As a new tyrannical ape leader builds his empire, one young ape undertakes a harrowing journey that will cause him to question all that he has known about the past and to make choices that will define a future for apes and humans alike.",
-    release_date: "May 5, 2024",
-    title: "Kingdom of the Planet of the Apes",
-    rating: 8.5
-  }
-  handleAddFavorite(title: string) {
-    console.log('handleAddFavorite', title);
-  }
-  handleAddWatchList(title: string) {
-    console.log('handleAddWatchList', title);
-  }
 
 }
---
+++ src/app/directives/app-max-lenght.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MaxLenghtDirective } from './app-max-lenght.directive';
+
+describe('AppMaxLenghtDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MaxLenghtDirective();
+    expect(directive).toBeTruthy();
+  });
+});
